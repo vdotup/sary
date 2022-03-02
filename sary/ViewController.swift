3eeff6b4052e1d44455cf2f0a8b204ed172f260f
vdotup <v.up@live.com>
2022-03-02 13:22:46 +0300

    Initial Commit

---
+++ sary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sary
+//
+//  Created by Abdurrahman Alfudeghi on 02/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
